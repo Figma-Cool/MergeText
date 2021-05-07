2a91e3ccf8904a0bf9ee34b553c513fed1ab066a
Yancy Min <senseslofter@gmail.com>
2021-05-07 18:14:17 +0800

    Initial commit

---
+++ code.js
@@ -0,0 +1,26 @@
+const selection = figma.currentPage.selection;
+let textItems = [];
+let newCharacters = [];
+
+selection.forEach((t) => {
+  if (t.type === "TEXT") {
+    textItems.push(t);
+    newCharacters.push(t.characters);
+  }
+});
+async function mergeText() {
+  await figma.loadFontAsync({
+    family: textItems[0].fontName.family,
+    style: textItems[0].fontName.style,
+  });
+  textItems[0].characters = newCharacters.join("\n");
+  textItems.map((t, i) => {
+    if (i !== 0) {
+      t.remove();
+    }
+  });
+  figma.notify("Done!");
+}
+
+mergeText();
+figma.closePlugin();
